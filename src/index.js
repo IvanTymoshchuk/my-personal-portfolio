ad1112884848ceb3bbbc9b6f88266e10f053d0ed
Ivan Tymoshchuk <timohin2231@gmail.com>
2023-10-14 11:37:19 +0200

    all

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { App } from 'components/App';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+   <BrowserRouter basename="/my-personal-portfolio">
+  <App />
+</BrowserRouter>
+  </React.StrictMode>
+);
